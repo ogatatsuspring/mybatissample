c4c98a65228a23157ba7d6662eb6cc309d798edc
ogawatatsuya <ogawatatsuya@localhost>
2025-08-10 13:06:12 +0900

    202508101306

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,20 +2,3 @@
 insert into books( title, author ) values( '新人研修あるある', '山田太郎' );
 insert into books( title, author ) values( 'こんなDBエンジニアは嫌だ', '佐藤花子' );
 insert into books( title, author ) values( '本当にあったIT業界トラブル', '鈴木一郎' );
-
-
--- Insert into Publishers
-insert into publishers( name ) values( '技術評論社' );
-insert into publishers( name ) values( 'Technology評論社' );
-
--- Insert into Books2
-insert into books2( title, author, publisher_id ) values( '新人研修あるある', '山田太郎', 1 );
-insert into books2( title, author, publisher_id ) values( 'こんなDBエンジニアは嫌だ', '佐藤花子', 2 );
-insert into books2( title, author, publisher_id ) values( '本当にあったIT業界トラブル', '鈴木一郎', 1 );
-
--- Insert into Reviews
-insert into reviews( book_id, review_text ) values( 1, '非常に役立つ本でした。' );
-insert into reviews( book_id, review_text ) values( 2, 'こんなDBエンジニアがいることに驚愕しました。' );
-insert into reviews( book_id, review_text ) values( 2, '自分の現場には来てほしくないです。' );
-insert into reviews( book_id, review_text ) values( 2, '同じ様な方が、私の現場にいます。。。' );
-insert into reviews( book_id, review_text ) values( 3, '業界の闇を知るにはいい本です。' );
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,29 +1,6 @@
--- Create table Books
+-- Create Table Books
 create table books (
 	id int primary key auto_increment,
 	title varchar( 255 ) not null,
 	author varchar( 255 ) not null
-);
-
--- Create Publishers Table
-create table publishers (
-	id int primary key auto_increment,
-	name varchar( 255 ) not null
-);
-
--- Create Books2 Table
-create table books2 (
-	id int primary key auto_increment,
-	title varchar( 255 ) not null,
-	author varchar( 255 ) not null,
-	publisher_id int,
-	foreign key ( publisher_id ) references publishers( id )
-);
-
--- Create Reviews Table
-create table reviews (
-	id int primary key auto_increment,
-	book_id int not null,
-	review_text text,
-	foreign key ( book_id ) references books2( id )
 );
